1f8a94bd96bbc2ae1e3fe9f2fe5f378370435d78
Felipe Marinho <felipmarinho@gmail.com>
2020-02-13 06:01:15 -0300

    Creating StyleSheet

--- App.js
+++ App.js
@@ -1,13 +1,23 @@
-import React, {Fragment} from 'react';
-import {Text} from 'react-native';
+import React from 'react';
+import {Image, ScrollView, Text, Dimensions, StyleSheet} from 'react-native';
 
+const largura = Dimensions.get('screen').width;
 const App = () => {
   return (
-    <Fragment>
+    <ScrollView>
       <Text>Felipe Marinho</Text>
+      <Image source={require('./res/img/alura.jpg')} style={estilo.imagem} />
       <Text>Felipe Marinho</Text>
-    </Fragment>
+      <Image source={require('./res/img/alura.jpg')} style={estilo.imagem} />
+    </ScrollView>
   );
 };
+
+const estilo = StyleSheet.create({
+  imagem: {
+    width: largura,
+    height: largura,
+  },
+});
 
 export default App;
